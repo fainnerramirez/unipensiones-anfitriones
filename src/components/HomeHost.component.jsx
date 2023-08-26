71828de593ac7c76dd271b82acc21ee5565d1b08
Fainner Ramirez <framirez@godoycordoba.com>
2023-08-26 16:48:21 -0500

    Update commit

--- src/components/HomeHost.component.jsx
+++ src/components/HomeHost.component.jsx
@@ -1,8 +1,9 @@
+import { Heading } from '@chakra-ui/react'
 import React from 'react'
 
 const HomeHost = () => {
   return (
-    <div>Home Anfitrión</div>
+    <Heading>Home Anfitrión</Heading>
   )
 }
 
