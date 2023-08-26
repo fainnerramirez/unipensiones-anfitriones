fd3e731e018c123ba9c5c12a93ee81cbc358df38
Fainner Ramirez <framirez@godoycordoba.com>
2023-08-26 16:45:09 -0500

    Initial commit

---
+++ src/components/HomeHost.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeHost = () => {
+  return (
+    <div>Home Anfitrión</div>
+  )
+}
+
+export default HomeHost
---
+++ src/utils/Theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource-variable/nunito"
+import "@fontsource/ubuntu"
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Nunito Variable', sans-serif;`,
+    body: `'Ubuntu', sans-serif`,
+  },
+});
+
+export default theme;
