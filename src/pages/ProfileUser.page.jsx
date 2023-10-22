caebf10884fbd14ddf6aac386df4be10ee8f243c
Fainner Ramirez <framirez@godoycordoba.com>
2023-10-21 19:39:16 -0500

    Update commit

--- src/pages/ProfileUser.page.jsx
+++ src/pages/ProfileUser.page.jsx
@@ -2,16 +2,14 @@ import { doc, getDoc } from 'firebase/firestore'
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { db } from '../firebase/firestore/database';
-import { Box, Image, Input, InputGroup, InputLeftElement, InputRightElement, Stack } from '@chakra-ui/react';
-import { CheckIcon, PhoneIcon } from '@chakra-ui/icons';
+import { Box, Heading, Image, VStack, Text } from '@chakra-ui/react';
 import { AuthContext } from '../context/authContext';
 
 const ProfileUser = () => {
   const { userId } = useParams();
   const [userDoc, setUserDoc] = useState(null);
+  const [anfitrion, setAnfitrion] = useState("Soy Anfitrión");
   const { userAuth } = useContext(AuthContext);
-
-  console.log("Userd Id Profile component: ", userId);
 
   useEffect(() => {
     const userDocRef = doc(db, "anfitriones", userId);
@@ -20,8 +18,6 @@ const ProfileUser = () => {
 
       if (docSnap.exists()) {
         setUserDoc(docSnap.data())
-        console.log("User query exit!", docSnap.data())
-
       } else {
         console.log("document not found!");
       }
@@ -31,56 +27,23 @@ const ProfileUser = () => {
   }, [userId])
 
   return (
-    <Stack spacing={4}>
+    <VStack align={'center'}>
       <Box display={'flex'} justifyContent={'center'}>
         <Image
           borderRadius={'full'}
-          boxSize='100px'
+          boxSize='350px'
           objectFit='cover'
           src={userAuth?.photoURL}
           alt={userAuth?.username}
         />
       </Box>
-      <InputGroup>
-        <InputLeftElement pointerEvents='none'>
-          <PhoneIcon color='gray.300' />
-        </InputLeftElement>
-        <Input type='tel' placeholder='Phone number' value={userDoc?.username} />
-      </InputGroup>
-
-      <InputGroup>
-        <InputLeftElement pointerEvents='none'>
-          <PhoneIcon color='gray.300' />
-        </InputLeftElement>
-        <Input type='tel' placeholder='Phone number' value={userDoc?.lastname} />
-      </InputGroup>
-
-      <InputGroup>
-        <InputLeftElement
-          pointerEvents='none'
-          color='gray.300'
-          fontSize='1.2em'
-          children='$'
-        />
-        <Input placeholder='Enter amount' value={userDoc?.userEmail} />
-        <InputRightElement>
-          <CheckIcon color='green.500' />
-        </InputRightElement>
-      </InputGroup>
-
-      <InputGroup>
-        <InputLeftElement
-          pointerEvents='none'
-          color='gray.300'
-          fontSize='1.2em'
-          children='$'
-        />
-        <Input placeholder='Enter amount' value={userDoc?.birthday} />
-        <InputRightElement>
-          <CheckIcon color='green.500' />
-        </InputRightElement>
-      </InputGroup>
-    </Stack>
+      <Box>
+        <Heading as="h2">{userDoc?.username + " " + userDoc?.lastname}</Heading>
+      </Box>
+      <Box>
+        <Text>{anfitrion}</Text>
+      </Box>
+    </VStack>
   )
 }
 
