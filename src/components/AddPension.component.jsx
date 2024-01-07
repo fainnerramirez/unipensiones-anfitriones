0d46bdf7fd8ac1d0506ca7342672b86ac5b3d6a7
Fainner Ramirez <framirez@godoycordoba.com>
2024-01-06 22:32:43 -0500

    update commit

--- src/components/AddPension.component.jsx
+++ src/components/AddPension.component.jsx
@@ -25,7 +25,13 @@ import {
     useDisclosure,
     useSteps,
     HStack,
-    Image
+    Image,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionIcon,
+    AccordionPanel,
+    Checkbox
 } from "@chakra-ui/react";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import imagen from "../assets/cardAfitrion.png"
@@ -93,36 +99,116 @@ const DatosBasicos = () => {
 }
 
 const Politicas = () => {
+
+    const [checkedItems, setCheckedItems] = React.useState([false, false])
+
+    const allChecked = checkedItems.every(Boolean)
+    const isIndeterminate = checkedItems.some(Boolean) && !allChecked
+
     return (
         <Stack spacing='10px' mt={10}>
-            <Box>
-                <Input
-                    placeholder='Escribe una politicas'
-                />
-            </Box>
-            <Box>
-                <Textarea id='desc' placeholder="Escribe una regla" />
-            </Box>
-            <HStack spacing={5}>
-                <Box>
-                    <Select id='owner' defaultValue='Seleccione e tipo de espacio'>
-                        <option value='segun'>Segun Adebayo</option>
-                        <option value='kola'>Kola Tioluwani</option>
-                    </Select>
-                </Box>
-                <Box>
-                    <Select id='owner' defaultValue='seleccione el tipo de alojamiento'>
-                        <option value='segun'>Segun Adebayo</option>
-                        <option value='kola'>Kola Tioluwani</option>
-                    </Select>
-                </Box>
-            </HStack>
-            <Box>
-                <Select id='owner' defaultValue='seleccione los servicios'>
-                    <option value='segun'>Segun Adebayo</option>
-                    <option value='kola'>Kola Tioluwani</option>
-                </Select>
-            </Box>
+            <Accordion allowToggle>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Normas de convivencia
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Checkbox
+                            isChecked={allChecked}
+                            isIndeterminate={isIndeterminate}
+                            onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+                        >
+                            Parent Checkbox
+                        </Checkbox>
+                        <Stack pl={6} mt={1} spacing={1}>
+                            <Checkbox
+                                isChecked={checkedItems[0]}
+                                onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+                            >
+                                Child Checkbox 1
+                            </Checkbox>
+                            <Checkbox
+                                isChecked={checkedItems[1]}
+                                onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+                            >
+                                Child Checkbox 2
+                            </Checkbox>
+                        </Stack>
+                    </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Normas de seguridad
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Checkbox
+                            isChecked={allChecked}
+                            isIndeterminate={isIndeterminate}
+                            onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+                        >
+                            Parent Checkbox
+                        </Checkbox>
+                        <Stack pl={6} mt={1} spacing={1}>
+                            <Checkbox
+                                isChecked={checkedItems[0]}
+                                onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+                            >
+                                Child Checkbox 1
+                            </Checkbox>
+                            <Checkbox
+                                isChecked={checkedItems[1]}
+                                onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+                            >
+                                Child Checkbox 2
+                            </Checkbox>
+                        </Stack>
+                    </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Normas de uso de espacios comunes
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Checkbox
+                            isChecked={allChecked}
+                            isIndeterminate={isIndeterminate}
+                            onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+                        >
+                            Parent Checkbox
+                        </Checkbox>
+                        <Stack pl={6} mt={1} spacing={1}>
+                            <Checkbox
+                                isChecked={checkedItems[0]}
+                                onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+                            >
+                                Child Checkbox 1
+                            </Checkbox>
+                            <Checkbox
+                                isChecked={checkedItems[1]}
+                                onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+                            >
+                                Child Checkbox 2
+                            </Checkbox>
+                        </Stack>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
         </Stack>
     );
 }
@@ -144,7 +230,7 @@ const AddPension = () => {
 
     const steps = [
         { title: 'Datos', description: 'Básicos' },
-        { title: 'Normas', description: 'Convivencia' },
+        { title: 'Normas', description: 'Reglas' },
         { title: 'Vista', description: 'Previa' },
     ];
 
@@ -173,7 +259,7 @@ const AddPension = () => {
                 <DrawerOverlay />
                 <DrawerContent>
                     <DrawerCloseButton />
-                    <DrawerHeader borderBottomWidth='1px'>Publica tu pensión</DrawerHeader>
+                    <DrawerHeader borderBottomWidth='1px'>Nueva publicación</DrawerHeader>
                     <DrawerBody>
                         <Stepper size={{ base: 'sm' }} index={activeStep}>
                             {steps.map((step, index) => (
